c1fed0eeafce8f06aafbb0125443b6079122f666
NousheenRazzak <nousheen461@gmail.com>
2024-09-08 18:12:12 +0500

    resume-builder

---
+++ app.js
@@ -0,0 +1,27 @@
+function clickMe() {
+    var button = document.getElementById("myDiv");
+    if (button.style.display === "none") {
+        button.style.display = "block";
+    }
+    else {
+        button.style.display = "none";
+    }
+}
+function clickMe2() {
+    var button = document.getElementById("myDiv2");
+    if (button.style.display === "none") {
+        button.style.display = "block";
+    }
+    else {
+        button.style.display = "none";
+    }
+}
+function clickMe3() {
+    var button = document.getElementById("myDiv3");
+    if (button.style.display === "none") {
+        button.style.display = "block";
+    }
+    else {
+        button.style.display = "none";
+    }
+}
---
+++ app.ts
@@ -0,0 +1,26 @@
+function clickMe() {
+    let button = document.getElementById("myDiv") as HTMLButtonElement;
+    if (button.style.display === "none") {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  }
+
+  function clickMe2() {
+    let button = document.getElementById("myDiv2") as HTMLButtonElement;
+    if (button.style.display === "none") {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  }
+
+  function clickMe3() {
+    let button = document.getElementById("myDiv3") as HTMLButtonElement;
+    if (button.style.display === "none") {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  }
